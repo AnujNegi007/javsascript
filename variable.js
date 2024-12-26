05dff630165557fe3ee6618585da640d11d84260
AnujNegi007 <anujnegi708@gmail.com>
2024-12-26 09:57:54 +0530

    learn variables

--- variable.js
+++ variable.js
@@ -1,2 +1,21 @@
 // To Print an statement in js we use this
 console.log("Hello World!")
+// beacuse of hositing i can change the value from here
+accountPassword = "100"
+
+accountState = "dehradun"
+// Variable & constant
+const accountId = 1
+let accountEmail = "anuj.negi@uber.com"
+var accountPassword;
+accountCity = "dehradun"
+let accountState;
+
+// accountId = 2 -> assingment in constnat varibale is not allowed if you try do that you will get an error
+
+// accountEmail = "anuj.negi@meta.com" if you do that the accountEmail will updated to anuj.negi@meta.com
+
+accountCity = "USA"
+
+// consle.table is used to show var in form of table
+console.table([accountId, accountEmail, accountPassword, accountCity])
