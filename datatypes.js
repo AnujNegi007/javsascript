bd34510e8ee920e964c189c0693592ac41d6a4cb
AnujNegi007 <anujnegi708@gmail.com>
2025-01-01 06:57:55 +0530

    learn datatypes 2nd lec

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+/* 
+Datatypes in javascript ->
+1. number -> int, negative int, float
+2. string -> always in double quotes "" or in '' single quotes
+3. boolean -> contain only 2 values true or false
+*/
+const number = -12
+console.log(typeof(number))
+
+const string = "anuj negi"
+console.log(typeof(string));
+
+const boolean = 0
+console.log(typeof(boolean));
+
+
