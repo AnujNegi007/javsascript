4a3adab30d6199e0670716de0188359937dc8f72
AnujNegi007 <anujnegi708@gmail.com>
2025-01-01 07:03:00 +0530

    add more in datatypes

--- datatypes.js
+++ datatypes.js
@@ -3,6 +3,10 @@ Datatypes in javascript ->
 1. number -> int, negative int, float
 2. string -> always in double quotes "" or in '' single quotes
 3. boolean -> contain only 2 values true or false
+4. null -> standalone value 
+5. symbol -> shows uniquenes
+6. undefiend -> when a value of a variable is not declared
+7. object -> primitve data type
 */
 const number = -12
 console.log(typeof(number))
@@ -12,5 +16,12 @@ console.log(typeof(string));
 
 const boolean = 0
 console.log(typeof(boolean));
+
+let a;
+console.log(typeof(a));
+
+let b = null;
+console.log(typeof(b));
+
 
 
